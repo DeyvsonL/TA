e3951c9e10220fba122993e4aa79f63be09a3ca5
DeyvsonL <dls4@cin.ufpe.br>
2015-06-10 18:44:36 -0300

    Correção de bug, métodos de interface não implementados

--- grails-app/controllers/ta/commom/DisciplineBuilder.groovy
+++ grails-app/controllers/ta/commom/DisciplineBuilder.groovy
@@ -13,4 +13,24 @@ class DisciplineBuilder implements IDisciplineBuilder {
 	public Discipline getDiscipline() {
 		return _Discipline
 	}
+
+	@Override
+	String getDisciplineName() {
+		return _Discipline.name
+	}
+
+	@Override
+	String getDisciplineCode() {
+		return _Discipline.code
+	}
+
+	@Override
+	void setDisciplineName(String name) {
+		_Discipline.name = name
+	}
+
+	@Override
+	void setDisciplineCode(String code) {
+		_Discipline.code = code
+	}
 }
